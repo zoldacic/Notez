701839bb3b655380b31c3918c52ee75d135fee69
Fredrik Lundgren <ext.fredrik.lundgren@tietoevry.com>
2023-03-11 23:55:33 +0100

    Started with project and chips

---
+++ MauiNotez/TodoSQLite/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace TodoSQLite.Models;
+
+public class TodoItem
+{
+    [PrimaryKey, AutoIncrement]
+    public int ID { get; set; }
+    public DateTime AssignedDate { get; set; } 
+    public string Name { get; set; }
+    public string Notes { get; set; }
+    public bool Done { get; set; }
+}
