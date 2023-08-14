c287cf4f11ca04235cdfdfbc4badc59493478c68
Fredrik Lundgren <ext.fredrik.lundgren@tietoevry.com>
2023-08-14 12:48:02 +0200

    .

--- MauiNotez/MauiBlazor/Data/TodoItemRepository.cs
+++ MauiNotez/MauiBlazor/Data/TodoItemRepository.cs
@@ -22,38 +22,38 @@ public class TodoItemRepository
         return await Repository.Database.Table<TodoItem>().Where(t => t.Done).ToListAsync();
     }
 
+    public async Task<List<TodoItem>> GetTemporaryItemsAsync()
+    {
+        return await Repository.Database.Table<TodoItem>().Where(t => t.IsTemporary).ToListAsync();
+    }
+
     public async Task<TodoItem> GetItemAsync(int id)
     {
         return await Repository.Database.Table<TodoItem>().Where(i => i.ID == id).FirstOrDefaultAsync();
     }
 
-    public async Task SaveItemsAsync(List<TodoItem> items, IList<Tag> tags)
-    {
-        foreach (var item in items) 
-        {
-            await SaveItemAsync(item, tags);
-        }
-    }
-
     public async Task SaveItemAsync(TodoItem item, IList<Tag> tags)
-    { 
+    {
+        var itemTagRepo = new ItemTagRepository();
+
         if (item.ID != 0)
         {
             await Repository.Database.UpdateAsync(item);
+
+            var currentTags = await itemTagRepo.GetItemTagsForItemAsync(item.ID);
+            var tagsToDelete = currentTags.Where(ct => !tags.Any(t => t.ID == ct.TagId)).ToList();
+
+            foreach (var tagToDelete in tagsToDelete)
+            {
+                await itemTagRepo.DeleteItemTagAsync(tagToDelete);
+            }
         }
         else
         {
             await Repository.Database.InsertAsync(item);
-        }
 
-        var itemTagRepo = new ItemTagRepository();
-
-        var currentTags = await itemTagRepo.GetItemTagsForItemAsync(item.ID);
-        var tagsToDelete = currentTags.Where(ct => !tags.Any(t => t.ID == ct.TagId)).ToList();
-
-        foreach (var tagToDelete in tagsToDelete)
-        {
-            await itemTagRepo.DeleteItemTagAsync(tagToDelete);
+            // Read back with Id set
+            item = await Repository.Database.Table<TodoItem>().OrderByDescending(t => t.ID).FirstOrDefaultAsync();
         }
 
         foreach (var tag in tags)
@@ -70,15 +70,33 @@ public class TodoItemRepository
 
     public async Task<int> DeleteAllTemporaryItemsAsync()
     {
-        var items = (await GetItemsAsync()).Where(i => i.IsTemporary);
+        var sql = "DELETE FROM TODOITEM WHERE ISTEMPORARY = TRUE";
+        return await Repository.Database.ExecuteAsync(sql);
 
-        var deleted = 0;
-        foreach(var item in items)
+        //var items = (await GetItemsAsync()).Where(i => i.IsTemporary);
+
+        //var deleted = 0;
+        //foreach(var item in items)
+        //{
+        //    deleted = deleted + await DeleteItemAsync(item);
+        //}
+
+        //return deleted;
+    }
+
+    public async Task CreateTemporaryItemsAsync(List<TodoItem> items, IList<Tag> tags)
+    {
+        await Repository.Database.InsertAllAsync(items);
+
+        items = (await GetItemsAsync()).Where(i => i.IsTemporary).ToList();
+
+        var itemTags = new List<ItemTag>();
+        foreach (var tag in tags)
         {
-            deleted = deleted + await DeleteItemAsync(item);
+            itemTags.AddRange(items.Select(i => new ItemTag() { ItemId = i.ID, TagId = tag.ID }));
         }
 
-        return deleted;
+        await Repository.Database.InsertAllAsync(itemTags);
     }
 
     public async Task<int> SetTemporaryItemsPermanentAsync()
